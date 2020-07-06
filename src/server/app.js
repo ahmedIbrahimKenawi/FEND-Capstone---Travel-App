d17ed589acc2836c8b62434e826725538c5b0177
ahmedIbrahimKenawi <35937041+ahmedIbrahimKenawi@users.noreply.github.com>
2020-07-06 02:58:23 +0200

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
+
 
 const app = express()
 
 /* Dependencies */
 const bodyParser = require('body-parser');
-const { json } = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 
-app.use(express.static('dist'))
+app.use(express.static(path.resolve(__dirname,'../../dist')))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
